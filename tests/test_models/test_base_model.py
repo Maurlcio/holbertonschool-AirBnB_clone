5498b086b5a65c206b6215ad3370a07e390bc8bd
DarianGrabino <dariangra@gmail.com>
2023-02-28 13:52:03 -0800

    tests base_model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class Test_BaseModel(unittest.TestCase):
+    def test_save(self):
+        bm = BaseModel()
+        old_updated_at = bm.updated_at
+        bm.save()
+        self.assertNotEqual(old_updated_at, bm.updated_at)
+
+    def test_to_dict(self):
+        bm = BaseModel()
+        bm_dict = bm.to_dict()
+        self.assertIsInstance(bm_dict, dict)
+        self.assertTrue('__class__' in bm_dict)
+        self.assertTrue('id' in bm_dict)
+        self.assertTrue('created_at' in bm_dict)
+        self.assertTrue('updated_at' in bm_dict)
+
+    def test_str(self):
+        bm = BaseModel()
+        bm_str = str(bm)
+        self.assertIsInstance(bm_str, str)
+        self.assertIn("[BaseModel]", bm_str)
+        self.assertIn("id", bm_str)
+        self.assertIn("created_at", bm_str)
+        self.assertIn("updated_at", bm_str)
+
+    def test_created_at(self):
+        bm = BaseModel()
+        creation_bm = bm.created_at
+        self.assertTrue(creation_bm == bm.created_at)
+
+if __name__ == "__main__":
+    unittest.main()
